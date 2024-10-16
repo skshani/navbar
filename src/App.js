6b000d23c4077cab532a52c640fd6abdc6ccbe4f
skshani <skshani4930@gmail.com>
2024-10-16 18:11:14 +0530

    added value

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NabPage from './component/NabPage'
+
+const App=() =>{
+  return (
+    <>
+<NabPage/>
+
+    </>
+  )
+}
+export default App
---
+++ src/component/NabPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./NabPage.css"
+
+const NabPage = () => {
+    return (
+        <>
+
+            <nav className='main-nav'>
+                <div className='menu'>
+                    <ul>
+                        <li><a href="#"></a>Home</li>
+                        <li><a href="#"></a>About</li>
+                        <li><a href="#"></a>Contact</li>
+                        <li><a href="#"></a>Services</li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div className='gallary'>
+
+            </div>
+            <section>
+                <p>this react page</p>
+                
+            </section>
+
+
+        </>
+    )
+}
+
+export default NabPage
